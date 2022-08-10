6d85144e0baf48bbc6a5e4a26b3270e6db0f85f1
Nakul Sagar <nakul.sagar@hyperrealitytech.com>
2022-08-10 19:53:03 +0530

    polling app

--- application/src/pages/home/index.tsx
+++ application/src/pages/home/index.tsx
@@ -1,15 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { FlatList } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { ActivityIndicator, FlatList, View, ViewStyle } from "react-native";
 import CardPage from "../../component/CardPage";
 import Api from "../../services/Api";
 
 import { useNavigation } from "@react-navigation/native";
 
 import { IData, HomeScreenNavigationProp } from "../../navigation/types";
+import { color, spacing } from "../../theme";
+
+const SEPARATOR: ViewStyle = {
+  height: 1,
+  backgroundColor: color.palette.lightGrey,
+};
+
+const LIST: ViewStyle = {
+  marginHorizontal: spacing[4],
+};
+
+const FOOTER: ViewStyle = {
+  marginBottom: spacing[4],
+};
 
 const Home = () => {
   const { navigate } = useNavigation<HomeScreenNavigationProp>();
-  const [page, setPage] = useState(0);
+  const [isEndLoading, setIsEndLoading] = useState(false);
+  let page = useRef(0);
   const [postData, setPostData] = useState<
     Array<{
       title: string;
@@ -21,16 +36,16 @@ const Home = () => {
 
   const callApi = async () => {
     try {
-      const response = await Api(page);
+      const response = await Api(page.current);
       setPostData((oldArray) => [...oldArray, ...response.data.hits]);
     } catch (err) {
       console.log(err);
     }
   };
 
-  useEffect(() => {
-    callApi();
-  }, [page]);
+  // useEffect(() => {
+  //   callApi();
+  // }, [page]);
 
   const renderItem = ({ item, index }) => (
     <CardPage
@@ -45,20 +60,37 @@ const Home = () => {
   );
 
   const onEndReached = () => {
-    setPage((old) => old + 1);
+    page.current = page.current + 1;
+    callApi();
   };
 
   useEffect(() => {
-    setInterval(() => {
+    callApi();
+    const interval = setInterval(() => {
+      page.current = page.current + 1;
       callApi();
+      // onEndReached();
     }, 10000);
-  }, [page]);
+  }, []);
+
+  const renderItemSeparator = () => <View style={SEPARATOR} />;
+  const renderFooter = () => (
+    <ActivityIndicator
+      size={"large"}
+      color={color.palette.lightGrey}
+      style={FOOTER}
+    />
+  );
 
   return (
     <FlatList
+      style={LIST}
       data={postData} //pass in our data array
       renderItem={renderItem} //tell React to use our renderItem function that we defined earlier
+      ItemSeparatorComponent={renderItemSeparator}
       onEndReached={onEndReached}
+      onEndReachedThreshold={0.3}
+      ListFooterComponent={renderFooter}
     />
   );
 };
